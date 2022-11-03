02be79bf07eadbfa7244b37985fa0b01362c4d17
web-Ali <38067660+web-Ali@users.noreply.github.com>
2022-11-03 03:46:35 +0300

    update

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,43 +1,22 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
-import {allPass, andThen, compose, ifElse, partial, prop, tap} from "ramda";
+import Api from '../tools/api';
+import {allPass, andThen, compose, ifElse, otherwise, partial, prop, tap} from "ramda";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
-const isMoreThanNumber = (a,b)=> +a < +b
-const isLessThanNumber = (a,b)=> +a > +b
-const isMoreThanTwo = partial(isMoreThanNumber,[2]);
-const isMoreThanZero = partial(isMoreThanNumber,[0]);
-const isLessThanTen = partial(isLessThanNumber,[10]);
+const isMoreThanNumber = (a, b) => +a < +b
+const isLessThanNumber = (a, b) => +a > +b
+
+const isMoreThanTwo = partial(isMoreThanNumber, [2]);
+const isMoreThanZero = partial(isMoreThanNumber, [0]);
+const isLessThanTen = partial(isLessThanNumber, [10]);
 
 const getStringLength = (string) => string.toString().length;
 const valuesLengthMoreThanTwo = compose(isMoreThanTwo, getStringLength)
-const valuesLengthMoreThanTen = compose(isLessThanTen,getStringLength)
+const valuesLengthMoreThanTen = compose(isLessThanTen, getStringLength)
 
 const isHaveOnlyNumberOrTouch = (string) => string.match(/[^.\d]/g) === null
 
-const isValidValue = allPass([valuesLengthMoreThanTen,valuesLengthMoreThanTwo, isMoreThanZero,isHaveOnlyNumberOrTouch])
+const isValidValue = allPass([valuesLengthMoreThanTen, valuesLengthMoreThanTwo, isMoreThanZero, isHaveOnlyNumberOrTouch])
 
 const roundValue = compose(Math.round, parseFloat);
 const squareIt = (n) => n ** 2;
@@ -45,30 +24,30 @@ const remainderByThree = (a) => a % 3;
 
 const getResult = prop('result');
 const thenGetResult = andThen(getResult);
+
 const convertNumberParams = (from, to, number) =>
-    api.get('https://api.tech/numbers/base')({from,to,number});
-const convertFrom10to2System = partial(convertNumberParams,[10,2])
+    api.get('https://api.tech/numbers/base')({from, to, number});
+const convertFrom10to2System = partial(convertNumberParams, [10, 2])
 const convertNumberRequest = compose(
     thenGetResult,
     convertFrom10to2System
 );
-const getAnimalById = (id) => api.get(`https://animals.tech/`)(id);
+
+const getAnimalById = (id) => api.get(`https://animals.tech/${id}`, {});
 const getAnimalByIdRequest = compose(
     thenGetResult,
-    getAnimalById
+    andThen(getAnimalById)
 );
 
 
 const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     const log = tap(writeLog)
+    const log = tap(writeLog)
     const logError = partial(handleError, ['ValidationError']);
-
+    const failedFetch = otherwise(handleError);
     const startProcess = compose(
+        failedFetch,
         andThen(handleSuccess),
-        andThen(getAnimalByIdRequest),
+        getAnimalByIdRequest,
         andThen(log),
         andThen(remainderByThree),
         andThen(log),
@@ -81,9 +60,7 @@ const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
         roundValue
     )
     const validatorAndStart = ifElse(isValidValue, startProcess, logError)
-
-    const result = compose(validatorAndStart,log)
+    const result = compose(validatorAndStart, log)
     result(value)
- }
-
- export default processSequence;
+}
+export default processSequence;
